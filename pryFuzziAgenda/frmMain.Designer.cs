4d5790da2894f13c66ff64431050f713d1ba7ef2
Fuszyy <milenbun@gmail.com>
2026-04-13 15:31:43 -0300

    Tab order, button functionality and input order :3

---
+++ pryFuzziAgenda/frmMain.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace pryFuzziAgenda
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblMail = new System.Windows.Forms.Label();
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.MaskedTextBox();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.cbxCategory = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.columnName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnSurname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnPhone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnMail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnCategory = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(221, 9);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(209, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Gestionar Contactos";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(208, 55);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(74, 16);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Nombre(s):";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(217, 122);
+            this.lblPhone.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(64, 16);
+            this.lblPhone.TabIndex = 5;
+            this.lblPhone.Text = "Teléfono:";
+            // 
+            // lblMail
+            // 
+            this.lblMail.AutoSize = true;
+            this.lblMail.Location = new System.Drawing.Point(231, 156);
+            this.lblMail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMail.Name = "lblMail";
+            this.lblMail.Size = new System.Drawing.Size(51, 16);
+            this.lblMail.TabIndex = 7;
+            this.lblMail.Text = "Correo:";
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(223, 87);
+            this.lblSurname.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(60, 16);
+            this.lblSurname.TabIndex = 3;
+            this.lblSurname.Text = "Apellido:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(293, 51);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(165, 22);
+            this.txtName.TabIndex = 2;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Enabled = false;
+            this.txtSurname.Location = new System.Drawing.Point(293, 83);
+            this.txtSurname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(131, 22);
+            this.txtSurname.TabIndex = 4;
+            this.txtSurname.TextChanged += new System.EventHandler(this.txtSurname_TextChanged);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Enabled = false;
+            this.txtPhone.Location = new System.Drawing.Point(292, 119);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPhone.Mask = "000-000-0000";
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(90, 22);
+            this.txtPhone.TabIndex = 6;
+            this.txtPhone.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txtPhone_MaskInputRejected);
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(211, 186);
+            this.lblCategory.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(69, 16);
+            this.lblCategory.TabIndex = 9;
+            this.lblCategory.Text = "Categoría:";
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxCategory.Enabled = false;
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.Items.AddRange(new object[] {
+            "Familia",
+            "Amigos",
+            "Trabajo",
+            "Otros.."});
+            this.cbxCategory.Location = new System.Drawing.Point(292, 183);
+            this.cbxCategory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(132, 24);
+            this.cbxCategory.TabIndex = 10;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(349, 454);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 28);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(457, 454);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(100, 28);
+            this.btnEdit.TabIndex = 13;
+            this.btnEdit.Text = "Editar";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(566, 454);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 28);
+            this.btnDelete.TabIndex = 14;
+            this.btnDelete.Text = "Eliminar";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnName,
+            this.columnSurname,
+            this.columnPhone,
+            this.columnMail,
+            this.ColumnCategory});
+            this.dataGridView1.Location = new System.Drawing.Point(10, 216);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(659, 228);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // columnName
+            // 
+            this.columnName.HeaderText = "Nombre(s)";
+            this.columnName.MinimumWidth = 6;
+            this.columnName.Name = "columnName";
+            this.columnName.Width = 125;
+            // 
+            // columnSurname
+            // 
+            this.columnSurname.HeaderText = "Apellido";
+            this.columnSurname.MinimumWidth = 6;
+            this.columnSurname.Name = "columnSurname";
+            this.columnSurname.Width = 125;
+            // 
+            // columnPhone
+            // 
+            this.columnPhone.HeaderText = "Teléfono";
+            this.columnPhone.MinimumWidth = 6;
+            this.columnPhone.Name = "columnPhone";
+            this.columnPhone.Width = 125;
+            // 
+            // columnMail
+            // 
+            this.columnMail.HeaderText = "Correo";
+            this.columnMail.MinimumWidth = 6;
+            this.columnMail.Name = "columnMail";
+            this.columnMail.Width = 125;
+            // 
+            // ColumnCategory
+            // 
+            this.ColumnCategory.HeaderText = "Categoría";
+            this.ColumnCategory.MinimumWidth = 6;
+            this.ColumnCategory.Name = "ColumnCategory";
+            this.ColumnCategory.Width = 125;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Enabled = false;
+            this.txtMail.Location = new System.Drawing.Point(290, 153);
+            this.txtMail.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(168, 22);
+            this.txtMail.TabIndex = 15;
+            this.txtMail.TextChanged += new System.EventHandler(this.txtMail_TextChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 490);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbxCategory);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.lblMail);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Agenda de Contactos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblMail;
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.MaskedTextBox txtPhone;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.ComboBox cbxCategory;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnSurname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnPhone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnMail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnCategory;
+        private System.Windows.Forms.TextBox txtMail;
+    }
+}
+
---
+++ pryFuzziAgenda/frmMain.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryFuzziAgenda
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+            txtSurname.Enabled = true;
+        }
+
+        private void txtSurname_TextChanged(object sender, EventArgs e)
+        {
+            txtPhone.Enabled = true;
+        }
+
+        private void txtPhone_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            txtMail.Enabled = true;
+        }
+
+
+        private void txtMail_TextChanged(object sender, EventArgs e)
+        {
+            cbxCategory.Enabled = true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" || txtSurname.Text == "" || txtPhone.Text == "" || txtMail.Text == "" || cbxCategory.Text == "")
+            {
+                MessageBox.Show("Complete los datos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Datos Guardados correctamente.", "Tarea Completada", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
